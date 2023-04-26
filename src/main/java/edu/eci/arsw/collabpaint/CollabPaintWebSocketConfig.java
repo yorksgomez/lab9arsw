30387208145d30d51593d34d609f619be7bddef4
Yorks Kevin Gómez López <yorksgomez@gmail.com>
2023-04-25 20:40:35 -0500

    Update CollabPaintWebSocketConfig.java

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -34,15 +34,13 @@ public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerCo
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.4").setRelayPort(61613);
         config.setApplicationDestinationPrefixes("/app");        
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
-        
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+    	registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();        
     }
-    
 
 }
